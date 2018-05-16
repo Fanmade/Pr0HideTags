ea3ce149869d52579a923c0fd83fd806fd3c7356
Ben <ben@fanmade.de>
2018-05-16 14:41:15 +0200

    Added settings which are stored via cookie

--- hide_tags.user.js
+++ hide_tags.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tags verstecken
-// @version      0.2.2
+// @version      0.8.0
 // @description  Versteckt die Tags um Spoiler zu vermeiden
 // @author       Selektion
 // @namespace    selektion
@@ -16,23 +16,119 @@
 // ==/UserScript==
 
 (function() {
-    waitForKeyElements (".tags", sel_tags_init);
-    function sel_tags_init($tagContainer) {
+    const selDefaultSetting = 1;
+    const selTagSettings = ["Nie","Immer","Nur bei Videos","Nur bei \"gegen Spoiler\" in den Tags"];
+    waitForKeyElements (".tags", selTagsInit);
+    function selBuildMenu(selectedSetting, returnHtml) {
+        let tagMenu = '<div class="sel-tag-menu"><a class="sel-close">x</a><h4>Tags verbergen:</h4>';
+        for(var i = 0;i < selTagSettings.length;i++) {
+            let checked = (i === selectedSetting);
+            tagMenu += '<li' + (checked ? ' class="active"' : '') + '><label>' + selTagSettings[i] + '<input type="radio" name="sel-tag-setting" value="' + i + '"' + (checked ? ' checked="checked"' : '') + '></label></li>';
+        }
+        tagMenu += '</ul></div>';
+        if (true === returnHtml) {
+            return tagMenu;
+        } else {
+            let $oldContainer = $('.sel-tag-menu');
+            $oldContainer.html($(tagMenu).html());
+        }
+    };
+    function getSetting(){
+        let cookieVal = $.cookie('sel-tags');
+        if (undefined == cookieVal) {
+            setSetting(selDefaultSetting);
+            return selDefaultSetting;
+        } else {
+            return parseInt(cookieVal);
+        }
+    }
+    function setSetting(val){
+        $.cookie("sel-tags", val,{path:'/'});
+    }
+    function showTags(){
+        let $tags = $('.tags');
+        let $tagToggle = $('.sel-tag-toggle');
+        $tags.addClass('show');
+        $tagToggle.text('Tags verbergen');
+    };
+    function hideTags(){
+        let $tags = $('.tags');
+        let $tagToggle = $('.sel-tag-toggle');
+        $tags.removeClass('show');
+        $tagToggle.text('Tags anzeigen');
+    };
+    function hasTag(searchTag, callback) {
+        let id = window.location.href.match(/[new/|top/]([0-9]{3,})/);
+        let result = false;
+        if(0 < id.length) {
+            id = parseInt(id[1]);
+            $.get( "http://pr0gramm.com/api/items/info?itemId=" + id, function( data ) {
+                if (undefined != data.tags) {
+                    $.each(data.tags, function(key,item) {
+                        if (undefined != item.tag) {
+                            if (item.tag.toLowerCase().indexOf(searchTag) >= 0) {
+                                result = true;
+                                return true;
+                            }
+                        }
+                    });
+                    if (!result) {
+                        callback.call();
+                    }
+                    return false;
+                }
+            });
+        } else {
+            return false;
+        }
+    }
+    function selTagsInit($tagContainer) {
         const style = document.createElement("style");
         style.type = "text/css";
-        style.innerHTML = 'span.tag{opacity:0.03;}span.tag:hover,.tags.show span.tag{opacity:1;}.tag-toggle{white-space:nowrap;}';
+        style.innerHTML = '.sel-nobreak{white-space:nowrap;position:relative;}span.tag{opacity:0.1;}a.tag-link{color:#f5f7f609;}.tags.show a.tag-link{color:#f5f7f6;}.tags.show span.tag{opacity:1;}a.sel-tag-settings.pict{font-size:10px;color:#ee4d2e;color:var(--theme-main-color);margin-left:4px;}a.sel-tag-settings.pict:hover{color:#f5f7f6;}.sel-tag-menu{opacity:.75;position:absolute;left:0;top:2em;z-index:100;border:3px solid #2a2e31;background-color:#161618;box-shadow:2px 0 10px #000;min-width:300px;max-height:50vh;}.sel-tag-menu h4{padding-bottom:3px;border-bottom:2px solid #252525;margin:0.3em;}.sel-tag-menu ul{list-style:none;pading-left:.3em;}.sel-tag-menu li{list-style:none;color:#666;}.sel-tag-menu li:hover{color:#ee4d2e;color:var(--theme-main-color);}.sel-tag-menu li.active{color:inherit;}.sel-tag-menu label{border-bottom:1px solid #252525;padding:2px 0 4px 4px;}.sel-tag-menu input[type="radio"]{display:none;}.sel-tag-menu{display:none;}.sel-tag-menu.visible{display:block;}.sel-close{position:absolute;right:0;top:0;padding:.3em;}';
         document.head.appendChild(style);
-        $tagContainer.append('<a class="tag-toggle action">Tags anzeigen</a>');
-        let $tagToggle = $('.tag-toggle');
+        let selSelectedSetting = getSetting();
+        $tagContainer.append('<span class="sel-nobreak"><a class="sel-tag-toggle action">Tags anzeigen</a><a class="sel-tag-settings pict" type="button">#</a>' + selBuildMenu(selSelectedSetting, true) + '</span>');
+        switch(selSelectedSetting){
+            case 0: //never
+                showTags();
+                break;
+            case 1: // always
+                hideTags();
+                break;
+            case 2: // only videos
+                hideTags();
+                hasTag('webm', showTags);
+                break;
+            case 3: // only spoilers
+                hideTags();
+                hasTag('gegen spoiler', showTags);
+                break;
+        }
+        const $menu = $('.sel-tag-menu');
         let $tags = $('.tags');
-        $tagContainer.on('click', '.tag-toggle', function(spoiler) {
+        $tagContainer.on('click', '.sel-tag-toggle', function() {
             if($tags.hasClass('show')) {
-                $tags.removeClass('show');
-                $tagToggle.text('Tags anzeigen');
+                hideTags();
             } else {
-                $tags.addClass('show');
-                $tagToggle.text('Tags verbergen');
+                showTags();
             }
         });
+        $tagContainer.on('change', 'input[type=radio]', function() {
+            selSelectedSetting = parseInt(this.value);
+            selBuildMenu(selSelectedSetting);
+            setSetting(selSelectedSetting);
+        });
+        $tagContainer.on('click', '.sel-close', function() {
+            $menu.removeClass('visible');
+        });
+        $tagContainer.on('click', '.sel-tag-settings', function() {
+            if ($menu.hasClass('visible')) {
+                $menu.removeClass('visible');
+            } else {
+                $menu.addClass('visible');
+            }
+        });
+
     }
 })();
